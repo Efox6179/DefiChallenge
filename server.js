294fe5c0d9e64646d6b35e3acdd06447397c1245
Efox6179 <95189308+Efox6179@users.noreply.github.com>
2022-03-30 19:38:41 -0500

    server working finally

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+const hbs = exphbs.create({});
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function (req,res){
+  res.render('homepage');
+});
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+  app.listen(PORT, () => console.log('Now listening'));
