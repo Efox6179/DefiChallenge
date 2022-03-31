38c30103b71a0e0033045ed0727026d8253f7095
McKenna <Mckennaburch19@gmail.com>
2022-03-30 19:48:04 -0500

    added user routes

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express'); // Include ExpressJS
+const app = express(); // Create an ExpressJS app
+const bodyParser = require('body-parser'); // middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+
+// route for user signup
+app.route('/signup')
+    //.get(sessionChecker, (req, res) => {
+    .get((req, res) => {
+        //res.sendFile(__dirname + '/public/signup.html');
+        res.render('signup', hbsContent);
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            //email: req.body.email,
+            password: req.body.password
+        })
+        .then(user => {
+            req.session.user = user.dataValues;
+            res.redirect('/dashboard');
+        })
+        .catch(error => {
+            res.redirect('/signup');
+        });
+    });
+
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        //res.sendFile(__dirname + '/public/login.html');
+        res.render('login', hbsContent);
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+		hbsContent.loggedin = true; 
+		hbsContent.userName = req.session.user.username; 
+		//console.log(JSON.stringify(req.session.user)); 
+		console.log(req.session.user.username); 
+		hbsContent.title = "You are logged in"; 
+        //res.sendFile(__dirname + '/public/dashboard.html');
+        res.render('index', hbsContent);
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+		hbsContent.loggedin = false; 
+		hbsContent.title = "You are logged out!"; 
+        res.clearCookie('user_sid');
+		console.log(JSON.stringify(hbsContent)); 
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// start the express server
+app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
+
+
