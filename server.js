a043ac07e89ce5bf026bf5f91a2fda21c2b0ede2
Efox6179 <95189308+Efox6179@users.noreply.github.com>
2022-04-02 21:09:23 -0500

    working login page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const app = express();
-
+const routes = require('./controllers')
 const hbs = exphbs.create({});
 const PORT = process.env.PORT || 3001;
 
@@ -16,7 +16,7 @@ app.get('/', function (req,res){
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')));
-//comment
+app.use(routes)
 
 
   app.listen(PORT, () => console.log('Now listening'));
