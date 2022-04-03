f75efc8a6afa888172528d7ba55b48825f3c2eef
SSto <95592688+sstover25@users.noreply.github.com>
2022-04-03 17:07:20 -0500

    trying

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const routes = require("./controllers/");
-const sequelize = require("./config/connections");
+const sequelize = require("./config/connection");
 const path = require("path");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-// const helpers = require("./utils/helpers");
+//const helpers = require("./utils/helpers");
 
 const sess = {
   secret: "place holder",
@@ -17,10 +17,10 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
 
 const exphbs = require("express-handlebars");
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.use(session(sess));
 
