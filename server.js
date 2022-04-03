5b6aaa39490d5e9641ab41f9be6e9131e907e76a
McKenna <Mckennaburch19@gmail.com>
2022-04-03 17:10:33 -0500

    add

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const exphbs = require('express-handlebars');
+const sequelize = require("./config/connection");
 const session = require('express-session')
 const routes = require('./controllers/')
 const path = require('path');
@@ -17,6 +18,7 @@ const sess = {
 };
 
 app.use(session(sess));
+
 
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
