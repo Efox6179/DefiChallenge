9fdade95b66d39618196c9c29164abc950ac0fc4
Efox6179 <95189308+Efox6179@users.noreply.github.com>
2022-04-03 17:39:15 -0500

    header updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const routes = require("./controllers/");
-const sequelize = require("./config/connections");
+const sequelize = require("./config/connection");
 const path = require("path");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -17,21 +17,26 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
 
 const exphbs = require("express-handlebars");
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({  });
 
 app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
+
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
