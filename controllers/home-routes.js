26c74fb1334a126b730fe74db9464ecdfcc57957
Quntarious <90361495+Quntarious@users.noreply.github.com>
2022-04-03 17:51:47 -0500

    solving error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,35 +7,9 @@ router.get("/login", (req, res) => {
 
 const { Post, User, Comment } = require("../models");
 
-// // GET all posts to display on the homepage
-// router.get("/", (req, res) => {
-//   console.log(req.session);
-//   Post.findAll({
-//     attributes: ["id", "title", "post_text", "created_at"],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-//         include: {
-//           model: User,
-//           attributes: ["username"],
-//         },
-//       },
-//       {
-//         model: User,
-//         attributes: ["username"],
-//       },
-//     ],
-//   })
-//     .then((dbPostData) => {
-//       const posts = dbPostData.map((post) => post.get({ plain: true }));
-//       res.render("homepage", { posts, loggedIn: req.session.loggedIn });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+router.get('/', function (req,res){
+  res.render('homepage'); 
+});
 
 // GET to send a user to the homepage if they are logged in. If they aren't logged in, then they are taken to the login page
 router.get("/login", (req, res) => {
