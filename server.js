2889da621285474953d7de30f875f97fcfe3e1ab
SSto <95592688+sstover25@users.noreply.github.com>
2022-04-03 16:24:47 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session')
-const routes = require('./controllers/')
-const path = require('path');
-const app = express();
-
-const PORT = process.env.PORT || 3005;
-
+const express = require("express");
+const routes = require("./controllers/");
+const sequelize = require("./config/connections");
+const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const helpers = require("./utils/helpers");
 
 const sess = {
-  secret: 'place holder',
+  secret: "place holder",
   cookie: {},
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
+
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+const exphbs = require("express-handlebars");
+// const hbs = exphbs.create({ helpers });
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log('Now listening'));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
