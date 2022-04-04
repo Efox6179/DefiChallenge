c46b9c2b21220e8668e546847bcdfb7ab3446ee6
SSto <95592688+sstover25@users.noreply.github.com>
2022-04-03 20:51:19 -0500

    added group-page handlebars, post-info partial, and helper utils

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require("./config/connection");
 const path = require("path");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-//const helpers = require("./utils/helpers");
+const helpers = require("./utils/helpers");
 
 const sess = {
   secret: "place holder",
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.use(session(sess));
 
