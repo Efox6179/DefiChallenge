7521e5d01b8dedf1c3bb2a29f74d1524769a40fb
McKenna <Mckennaburch19@gmail.com>
2022-04-05 21:15:16 -0500

    test

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3008;
 
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
